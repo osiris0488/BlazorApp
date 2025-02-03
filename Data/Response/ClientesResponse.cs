3eaff15ca471fb13db727d8ccc5fc11f0ab69bbd
ESTHERLIN OSIRIS DOMINGUEZ GAVILAN <2019-0488@uteco.edu.do>
2025-02-03 10:26:35 -0400

    cambios

---
+++ Data/Response/ClientesResponse.cs
@@ -0,0 +1,30 @@
+﻿using Alquilandome.Data.Request;
+using BlazorApp.Data.Entities;
+using BlazorApp.Data.Request;
+
+namespace Alquilandome.Data.Response
+{
+
+    // Clase Cliente
+    public class ClienteResponse
+    {
+		public int ClienteId { get; set; }
+		public required string Nombre { get; set; }
+		public required string Email { get; set; }
+		public required string Password { get; set; }
+		public List<Cita> Citas { get; set; } = new List<Cita>();
+
+		public ClienteResponse ToRequest()
+        {  
+            return new ClienteRequest
+            { 
+                Id= ClienteId,
+                Nombre = Nombre, 
+                Email = Email, 
+                Password = Password, 
+                
+            }; 
+        }
+    }
+
+}
---
+++ Data/Resquest/ClientesRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorApp.Data.Request;
+    public class ClienteRequest
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = null!;
+        [Required(ErrorMessage = "La nombre del cliente es obligatoria")]
+        public string Email { get; set; } = null!;
+        [Required(ErrorMessage = "El email es obligatorio")]
+        public string? Password { get; set; }
+      
+    }
